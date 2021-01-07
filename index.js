d858f1bdfa4c8df4b82334d811ce8f990e076187
M45um19 <73242093+M45um19@users.noreply.github.com>
2021-01-07 11:43:17 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.btn').click(function(){
+      $('.items').toggleClass("show");
+      $('ul li').toggleClass("hide");
+    });
+  });
+
+
+
+
+  var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+	 
+// Image List
+images[0] = "image/111.jpg";
+images[1] = "image/222.jpg";
+
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
